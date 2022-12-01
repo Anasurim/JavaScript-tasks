b1c7fee81943a287d1b8aa98769759d441fb96ee
Lebedynskyi Roman <cliffcrystall@gmail.com>
2022-12-01 03:20:45 +0100

    just trained with the variables

---
+++ js/task1.js
@@ -0,0 +1,24 @@
+let username;
+console.log(typeof username);
+
+let inputValue = null;
+console.log(typeof inputValue);
+
+const quantity = 17;
+console.log(typeof quantity);
+
+const message = "Hello World!";
+console.log(typeof message);
+
+const isSideBarOpen = false;
+console.log(typeof isSideBarOpen);
+
+// next task
+
+const newMessage = "Hello World!";
+console.log(newMessage);
+
+username = "Roman";
+console.log("My name is", username);
+
+alert(message);
