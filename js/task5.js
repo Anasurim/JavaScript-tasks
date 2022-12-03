7c0dfe67788f34e2bb3a3449338b78dc519c3e04
Lebedynskyi Roman <cliffcrystall@gmail.com>
2022-12-03 14:22:00 +0100

    task5 done

---
+++ js/task5.js
@@ -0,0 +1,17 @@
+const age = 32;
+console.log(age > 10 && age < 40);
+console.log(age > 9 && age > 33);
+
+console.log(1 && 5);
+console.log(5 && 1);
+console.log("MAngo" && "");
+console.log("Mango" && "Poly");
+
+const year = 1989;
+console.log(year < 2000 || year < 1500);
+console.log(year < 0 || year > 2000);
+console.log(true || "");
+
+console.log(!true);
+console.log(!false);
+console.log(!"");
