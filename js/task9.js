71372f56b19312b7d06650a1aca25065aee73553
Lebedynskyi Roman <cliffcrystall@gmail.com>
2022-12-08 04:13:53 +0100

    some tasks done

---
+++ js/task9.js
@@ -0,0 +1,11 @@
+// for (let i = 100; i > 2; i -= 2) {
+//   console.log(i);
+// }
+
+function numberToString(num) {
+  // Return a string of the number here!
+
+  return num.toString();
+}
+
+console.log(typeof numberToString(5));
