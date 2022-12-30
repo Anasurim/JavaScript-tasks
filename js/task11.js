c679049e85af527335e7c5fc5253b05226fbfd3c
Lebedynskyi Roman <cliffcrystall@gmail.com>
2022-12-30 08:46:05 +0100

    updated task11

--- js/task11.js
+++ js/task11.js
@@ -1,5 +1,29 @@
+// const clients = ["Poly", "Roman", "Nelya"];
+
+// for (let i = 0; i < clients.length; i += 1) {
+//   console.log(clients[i]);
+// }
+
 const clients = ["Poly", "Roman", "Nelya"];
 
-for (let i = 0; i < clients.length; i += 1) {
-  console.log(clients[i]);
+// for (const client of clients) {
+//   console.log(client);
+// }
+
+const string = "Javascript";
+for (const character of string) {
+  console.log(character);
 }
+
+const nameToFind = "Roman";
+let message;
+
+for (const client of clients) {
+  if (client === nameToFind) {
+    message = "Client was found";
+
+    break;
+  }
+  message = "Client was not found";
+}
+console.log(message);
