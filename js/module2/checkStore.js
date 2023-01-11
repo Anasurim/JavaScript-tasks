ca9c1351bb35fec19a4839c4a349530bb72f10d9
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-01-11 21:13:38 +0100

    task check the storage complete

---
+++ js/module2/checkStore.js
@@ -0,0 +1,12 @@
+function checkStorage(available, ordered) {
+  if (ordered === 0) {
+    return "Your order is empty!";
+  }
+  if (ordered > available) {
+    return "Your order is too large, not enough goods in stock!";
+  }
+
+  return "The order is accepted, our manager will contact you";
+}
+
+console.log(checkStorage(100, 190));
