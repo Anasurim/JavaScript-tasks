a9b572f69010b8dfcb6b5730c2509a049adcd5ae
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-01-11 11:36:15 +0100

    method push and pop

---
+++ js/module2/push&pop.js
@@ -0,0 +1,37 @@
+// push
+
+const numbers = [];
+console.log(numbers);
+
+numbers.push(1);
+console.log(numbers);
+
+numbers.push(2);
+console.log(numbers);
+
+numbers.push(3);
+console.log(numbers);
+
+numbers.push(4);
+console.log(numbers);
+
+numbers.push(5);
+console.log(numbers);
+
+// pop
+
+const numbersNotEmpty = [1, 2, 3, 4, 5];
+numbersNotEmpty.pop();
+console.log(numbersNotEmpty);
+
+numbersNotEmpty.pop();
+console.log(numbersNotEmpty);
+
+numbersNotEmpty.pop();
+console.log(numbersNotEmpty);
+
+numbersNotEmpty.pop();
+console.log(numbersNotEmpty);
+
+numbersNotEmpty.pop();
+console.log(numbersNotEmpty);
