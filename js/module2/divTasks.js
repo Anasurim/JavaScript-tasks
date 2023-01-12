875269c271a4a23e3efd1aede83b1be617ce02c5
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-01-12 13:14:34 +0100

    arraysTasks

---
+++ js/module2/divTasks.js
@@ -0,0 +1,11 @@
+const fruits = ["apple", "plum", "pear", "orange"];
+console.log(fruits);
+// Write your code under this line
+fruits[1] = "peach";
+fruits[3] = "banana";
+console.log(fruits);
+
+const lastElementIndex = fruits.length - 1;
+console.log(lastElementIndex);
+const lastElement = fruits[lastElementIndex];
+console.log(lastElement);
