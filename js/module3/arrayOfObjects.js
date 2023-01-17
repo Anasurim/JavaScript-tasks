061237fe0879c68846626044ecdb720ab00f6549
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-01-17 03:09:40 +0100

    add here function that in the beginning  didnit  understand

--- js/module3/arrayOfObjects.js
+++ js/module3/arrayOfObjects.js
@@ -37,3 +37,23 @@ console.log(bookAuthor);
 
 const averageRating = (totalRating / books.length).toFixed(2);
 console.log(averageRating);
+
+// ======
+console.log("================");
+
+function printContacts(string1, string2) {
+  const names = string1.split(",");
+  const phones = string2.split(",");
+
+  for (const name of names) {
+    const index = names.indexOf(name);
+    const phone = phones[index];
+
+    console.log(`${name}: ${phone}`);
+  }
+
+  // console.log(names);
+  // console.log(phones);
+}
+
+printContacts("Roman,Nataliia,Wladimir", "0204040505,03003003,00450500560");
