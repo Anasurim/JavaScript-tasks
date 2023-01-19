429dffdbb33b81797785d225543b3b6947b86103
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-01-19 14:43:00 +0100

    created pricecalc

---
+++ js/module3/calculateTotalPrice.js
@@ -0,0 +1,17 @@
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  for (let product of products) {
+    if (product.name === productName) {
+      return product.price * product.quantity;
+    }
+    return 0;
+  }
+}
+
+console.log(calculateTotalPrice("Rada"));
