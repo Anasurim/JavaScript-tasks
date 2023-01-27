095fe24752027cca798955ccb8fec2de91138370
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-01-27 17:06:08 +0100

    task bookStoring to practice: create some solutions

---
+++ js/module4/bookSorting.js
@@ -0,0 +1,44 @@
+const books = [
+  {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    rating: 8.38,
+  },
+  {
+    title: "Beside Still Waters",
+    author: "Robert Sheckley",
+    rating: 8.51,
+  },
+  {
+    title: "The Dream of a Ridiculous Man",
+    author: "Fyodor Dostoevsky",
+    rating: 7.75,
+  },
+  { title: "Redder Than Blood", author: "Tanith Lee", rating: 7.94 },
+  { title: "Enemy of God", author: "Bernard Cornwell", rating: 8.67 },
+];
+
+const ByAuthorName = [...books].sort((firstAuthor, secondAuthor) =>
+  firstAuthor.author.localeCompare(secondAuthor.author)
+);
+
+const sortedByAuthorName = ByAuthorName.map(({ title }) => title);
+
+console.log(sortedByAuthorName);
+
+console.table(books);
+// const sortedByReversedAuthorName = books.sort((firstAuthor, secondAuthor) =>
+//   secondAuthor.author.localeCompare(firstAuthor.author)
+// );
+
+const sortedByAscendingRating = [...books]
+  .sort((a, b) => a.rating - b.rating)
+  .map(({ title }) => title);
+
+console.log(sortedByAscendingRating);
+
+const sortedByDescentingRating = [...books]
+  .sort((a, b) => b.rating - a.rating)
+  .map(({ title }) => title);
+
+console.log(sortedByDescentingRating);
