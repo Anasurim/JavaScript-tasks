ddbe283dd52f98e53b0eb15d334094656e5ca86f
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-01-27 19:48:04 +0100

    task bookStoring to practice: sort+filter+map

--- js/module4/bookSorting.js
+++ js/module4/bookSorting.js
@@ -24,7 +24,7 @@ const ByAuthorName = [...books].sort((firstAuthor, secondAuthor) =>
 
 const sortedByAuthorName = ByAuthorName.map(({ title }) => title);
 
-console.log(sortedByAuthorName);
+// console.log(sortedByAuthorName);
 
 console.table(books);
 // const sortedByReversedAuthorName = books.sort((firstAuthor, secondAuthor) =>
@@ -35,10 +35,23 @@ const sortedByAscendingRating = [...books]
   .sort((a, b) => a.rating - b.rating)
   .map(({ title }) => title);
 
-console.log(sortedByAscendingRating);
+// console.log(sortedByAscendingRating);
 
 const sortedByDescentingRating = [...books]
   .sort((a, b) => b.rating - a.rating)
   .map(({ title }) => title);
 
-console.log(sortedByDescentingRating);
+// console.log(sortedByDescentingRating);
+
+console.log("==============sort + filter + map===========");
+
+const MIN_BOOK_RATING = 8;
+
+const names = books
+  .filter((rate) => rate.rating > MIN_BOOK_RATING)
+  .map(({ author }) => author)
+  .sort((a, b) => a.localeCompare(b));
+//   .map(({ author }) => author)
+//   .sort((a, b) => a.author.localeCompare(b.author));
+
+console.log(names);
