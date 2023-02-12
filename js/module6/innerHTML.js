3689f7371840e946dc26dfc8728c2d793e78462d
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-02-12 23:02:51 +0100

    DOM, innerHTML, createElement(), insertAdjacentHTML

--- js/module6/innerHTML.js
+++ js/module6/innerHTML.js
@@ -1,10 +1,31 @@
 const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
 const list = document.querySelector(".list");
 
-const markup = technologies
-  .map((technology) => `<li class="list-item">${technology}</li>`)
+// const markup = technologies
+//   .map((technology) => `<li class="list-item">${technology}</li>`)
+//     .join("");
+
+const newTechnologies = ["React", "TypeScript", "Node.js"];
+const markup = newTechnologies
+  .map((technology) => `<li class="list-item new">${technology}</li>`)
   .join("");
 
+list.insertAdjacentHTML("beforeend", markup);
+list.insertAdjacentHTML(
+  "beforebegin",
+  "<h2>Updated stack of technologies</h2>"
+);
 console.log(markup);
 
-list.innerHTML = markup;
+//list.innerHTML = markup;
+
+// ==============
+
+const article = document.querySelector(".article");
+const htmlString = `<p class="article-text">Nullam quis ante. Vestibulum dapibus nunc ac augue. In consectetuer turpis ut velit.</p>
+   <a class="link" href="#">Read more...</a>`;
+
+// Replace += with = operator. See the difference?
+// Article title is lost because we overwrite element content.
+article.innerHTML += htmlString; //correct
+// article.innerHTML = htmlString;    //incorrect
