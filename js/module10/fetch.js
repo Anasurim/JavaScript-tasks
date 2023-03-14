11397e6c71c0d07c649e5b667c4ff300fc6492d0
Lebedynskyi Roman <cliffcrystall@gmail.com>
2023-03-14 20:46:50 +0100

    rest api and fetch to practice

---
+++ js/module10/fetch.js
@@ -0,0 +1,13 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // Data handling
+  })
+  .catch((error) => {
+    // Error handling
+  });
